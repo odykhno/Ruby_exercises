6d4d9d0f5668672b72ea5430b30178276882df1a
Olena Dykhno <olenadykhno@gmail.com>
2016-07-06 14:51:15 +0300

    add the first part of list example

--- lib/spec/prefix_tree_spec.rb
+++ lib/spec/prefix_tree_spec.rb
@@ -7,7 +7,7 @@ describe PrefixTree do
 
   describe "#new" do
     it "takes no parameters and returns a PrefixTree object" do
-      @prefix_tree.should be_an_instance_of PrefixTree
+      expect(@prefix_tree).to be_an_instance_of PrefixTree
     end
   end
 
@@ -17,15 +17,36 @@ describe PrefixTree do
     end
     describe "add provided string in the tree" do
       it "provided string is present in the tree" do
-        @prefix_tree.include?("card").should be true
+        expect(@prefix_tree.include?("card")).to be true
       end
       it "not provided string is not present in the tree" do
-        @prefix_tree.include?("car").should be false
+        expect(@prefix_tree.include?("car")).to be false
       end
       it "not provided string is not present in the tree" do
-        @prefix_tree.include?("cardy").should be false
+        expect(@prefix_tree.include?("cardy")).to be false
       end
     end
+  end
+
+  describe "#list" do
+    before do
+      @prefix_tree.add "card"
+      @prefix_tree.add "table"
+      @prefix_tree.add "cardigan"
+      @list = @prefix_tree.list
+    end
+    describe "if there is no provided prefix" do
+      it "returns list of words with 5 or more then letters" do
+        expect(@list.size).to eq(2)
+      end
+      it "should contain provided words exactly" do
+        expect(@list).to include("table", "cardigan")
+      end
+      it "should not contain words with less then 5 letters" do
+        expect(@list).to_not include("card")
+      end
+    end
+
   end
 
 end
