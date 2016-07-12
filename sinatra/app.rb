9741c9e94618e9c361fe29327b78b22fde7514e1
Olena Dykhno <olenadykhno@gmail.com>
2016-07-12 22:54:12 +0300

    add sinatra app with started page and add, include, list methods

---
+++ sinatra/app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require '../lib/prefix_tree'
+
+$pr_tr = PrefixTree.new
+
+get '/' do
+  erb :index
+end
+
+post '/add' do
+  word = params[:word_add]
+  $pr_tr.add word
+  redirect '/'
+end
+
+get '/include' do
+  word = params[:word_check]
+  $pr_tr.include? word
+  redirect '/'
+end
+
+get '/list' do
+  prefix = params[:prefix]
+  if prefix
+    output = $pr_tr.list prefix
+  else
+    output = $pr_tr.list
+  end
+  redirect '/'
+end
+
+
